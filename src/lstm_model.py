14c66334a93d5030ac5d24b3784c98202bb9415e
hyndaviponnuru63-droid <hyndaviponnuru63@gmail.com>
2026-01-13 20:05:17 +0530

    Update lstm_model.py

--- src/lstm_model.py
+++ src/lstm_model.py
@@ -4,24 +4,22 @@ from tensorflow.keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
 
 def train_lstm(df):
-    """
-    df: preprocessed dataframe for LSTM
-    """
     scaler = MinMaxScaler()
-    X = scaler.fit_transform(df.drop(['death_inhosp'], axis=1))
+    X = df.drop(['death_inhosp'], axis=1).values
     y = df['death_inhosp'].values
-
-    # Reshape for LSTM [samples, timesteps, features]
-    X = X.reshape((X.shape[0], 1, X.shape[1]))
+    
+    X_scaled = scaler.fit_transform(X)
+    X_scaled = X_scaled.reshape((X_scaled.shape[0], 1, X_scaled.shape[1]))
 
     model = Sequential()
-    model.add(LSTM(32, input_shape=(X.shape[1], X.shape[2])))
+    model.add(LSTM(32, input_shape=(X_scaled.shape[1], X_scaled.shape[2])))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X, y, epochs=5, batch_size=16, verbose=0)
+    model.fit(X_scaled, y, epochs=5, batch_size=16, verbose=0)
     return model, scaler
 
 def predict_lstm(model, scaler, df_row):
-    X = scaler.transform(df_row)
-    X = X.reshape((X.shape[0], 1, X.shape[1]))
-    return model.predict(X)[0][0]
+    X = df_row.values.reshape(1, -1)
+    X_scaled = scaler.transform(X)
+    X_scaled = X_scaled.reshape((X_scaled.shape[0], 1, X_scaled.shape[1]))
+    return model.predict(X_scaled, verbose=0)[0][0]
